17600a9f7d40cca4fad61385e0a3342ce938e92c
WilliamViniciusOliveiradaRosa <williamdeoliveira360@outlook.com>
2025-07-27 17:53:52 -0300

    add get post cors
    teste para rodar cors

--- index.js
+++ index.js
@@ -6,11 +6,17 @@ const cors = require('cors');  // Adicionando a importação do cors
 // Define a porta que a API vai escutar
 const PORT = process.env.PORT || 3000;
 
-// Permitir CORS de qualquer origem
-app.use(cors({ origin: '*' }));
-
+// Permitir CORS de qualquer origem, com configuração de métodos e cabeçalhos
+app.use(cors({
+  origin: '*', // Aceita qualquer origem
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],  // Métodos permitidos
+  allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
+  credentials: true // Se for necessário enviar cookies ou cabeçalhos
+}));
 // Middleware para aceitar JSON no corpo das requisições
 app.use(express.json());
+
+
 
 // Rota de exemplo: GET
 app.get('/', (req, res) => {
